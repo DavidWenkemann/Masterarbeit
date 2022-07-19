ed3800fe8488352727750bddb41be54b33480775
David Wenkemann <david.wenkemann@fh-erfurt.de>
2022-07-19 22:20:25 +0200

    add basedata documentation

--- basedata/basedata.go
+++ basedata/basedata.go
@@ -1,3 +1,6 @@
+//basedata package is responsible for adding and removing basedata.
+//Its connected to DB and UI
+
 package main
 
 import (
@@ -5,6 +8,7 @@ import (
 	"github.com/DavidWenkemann/Masterarbeit/model"
 )
 
+//adds new product to DB
 func addProduct(ean string, name string, price float64) model.BProduct {
 	var p model.BProduct
 	if database.GetProductByEan(ean) != nil {
@@ -13,6 +17,7 @@ func addProduct(ean string, name string, price float64) model.BProduct {
 	return p
 }
 
+//Removes specific product from DB by EAN
 func removeProduct(ean string) {
 	database.RemoveProductByEan(ean)
 }
